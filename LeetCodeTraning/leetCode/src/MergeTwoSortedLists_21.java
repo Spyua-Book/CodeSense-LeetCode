b134e3006e019235c2acc7fe59fdbd2335a63e1d
spyua <pal2097@gmail.com>
2021-10-12 16:15:47 +0800

    New: New Week Homework

---
+++ LeetCodeTraning/leetCode/src/MergeTwoSortedLists_21.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+public class MergeTwoSortedLists_21 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        var mergeListNode = new ListNode();
+        return mergeListNode;
+    }
+}
